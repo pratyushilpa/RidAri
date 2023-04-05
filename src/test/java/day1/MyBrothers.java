96e2c328a7836111ba32a94223ec027c3107dbe5
pratyushilpa <pratyush87singh@gmail.com>
2023-04-05 23:19:19 +0530

    Initial commit Day1

---
+++ src/test/java/day1/MyBrothers.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class MyBrothers {
+    public static void main(String[] args) {
+        System.out.println("Ridvan is my younger brother");
+        System.out.println("Ari is my elder brother");
+    }
+}
